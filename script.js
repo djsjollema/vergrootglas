9ce9d1a99f7a2fbd92b3e3a8037848d4b0d5a618
Jelle Sjollema <djsjollema@gmail.com>
2016-03-29 09:05:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",function(){ 
+	var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var mouseX, mouseY;
+    var image = new Image();
+    image.src = "middleearthlargelargerstill.jpg"; 
+})
